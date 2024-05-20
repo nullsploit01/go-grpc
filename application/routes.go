d13dc0631f7b3372bf2343fd379a93b1a2809e69
nullsploit01 <harshaldharmik099@gmail.com>
2024-05-21 02:02:24 +0530

    setup basic order routes

--- application/routes.go
+++ application/routes.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/nullsploit01/go-microservice/handler"
 )
 
 func loadRoutes() *chi.Mux {
@@ -16,5 +17,22 @@ func loadRoutes() *chi.Mux {
 		w.WriteHeader(http.StatusOK)
 	})
 
+	router.Get("/ping", pong)
+	router.Route("/orders", loadOrderRoutes)
+
 	return router
+}
+
+func loadOrderRoutes(router chi.Router) {
+	orderHandler := &handler.Order{}
+
+	router.Post("/", orderHandler.Create)
+	router.Get("/", orderHandler.List)
+	router.Get("/{id}", orderHandler.GetById)
+	router.Put("/{id}", orderHandler.UpdateById)
+	router.Delete("/{id}", orderHandler.DeleteById)
+}
+
+func pong(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Pong!"))
 }
